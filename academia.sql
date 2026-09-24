51e27797aa2e8e59d1fc8c24a8b4f433a8995397
Manuela Urbano <manuela_m_silva@estudante.sesisenai.org.br>
2026-09-24 10:56:27 -0300

    24/09

--- academia.sql
+++ academia.sql
@@ -74,3 +74,52 @@ SELECT
 FROM modalidades m
 JOIN planos p ON m.plano_id = p.id
 ORDER BY mensalidade_com_taxa DESC;
+
+CREATE VIEW vw_relacao2 AS
+SELECT 
+    a.nome AS aluno, 
+    a.cpf, 
+    mod.nome AS modalidade, 
+    mod.sala, 
+    im.duracao_meses, 
+    mat.data_inicio
+FROM matriculas mat
+JOIN alunos a ON mat.aluno_id = a.id
+JOIN itens_matricula im ON mat.id = im.matricula_id
+JOIN modalidades mod ON im.modalidade_id = mod.id
+WHERE mat.status = 'Ativa';
+
+CREATE VIEW vw_relacao3 AS
+SELECT 
+    a.nome, 
+    COUNT(mat.id) AS total_matriculas, 
+    SUM((im.valor_mensal_aplicado * im.duracao_meses) + im.taxa_adesao) AS total_investido
+FROM alunos a
+JOIN matriculas mat ON a.id = mat.aluno_id
+JOIN itens_matricula im ON mat.id = im.matricula_id
+WHERE mat.status = 'Ativa'
+GROUP BY a.id, a.nome
+HAVING SUM((im.valor_mensal_aplicado * im.duracao_meses) + im.taxa_adesao) > 1000.00;
+
+CREATE VIEW vw_relacao4 AS
+SELECT 
+	m.*, 
+	p.nome as plano,
+	p.valor_mensal_base
+FROM modalidades m
+JOIN planos p ON m.plano_id = p.id
+WHERE m.capacidade_maxima >= 15 
+  AND p.valor_mensal_base > 100.00 
+  AND m.disponivel = TRUE;
+
+CREATE VIEW vw_relacao5 AS
+SELECT 
+    p.nome,
+    SUM((im.valor_mensal_aplicado * im.duracao_meses) + im.taxa_adesao) AS faturamento_total,
+    ROUND(AVG(im.duracao_meses), 1) AS media_meses_contratados
+FROM itens_matricula im
+JOIN matriculas mat ON im.matricula_id = mat.id
+JOIN modalidades mod ON im.modalidade_id = mod.id
+JOIN planos p ON mod.plano_id = p.id
+WHERE mat.status = 'Ativa'
+GROUP BY p.nome;
